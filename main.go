ea2d50d57686f286ca73a0593892031160442bd6
Togo Enomoto <t.enomoto@catallaxy.me>
2024-03-23 19:51:51 +0900

    encoding

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"io"
-	"net"
+	"encoding/json"
 	"os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "example.com:80")
-	if err != nil {
-		panic(err)
-	}
-	io.WriteString(conn, "GET / HTTP/1.0\r\nHost: example.com\r\n\r\n")
-	io.Copy(os.Stdout, conn)
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", " ")
+	encoder.Encode(map[string]string{
+		"example": "encoding/json",
+		"hello":   "world"})
 }
