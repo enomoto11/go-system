3f068f03324d08914724d56cc5565e1724d8ced4
Togo Enomoto <t.enomoto@catallaxy.me>
2024-04-06 18:14:44 +0900

    chunk

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"hash/crc32"
 	"io"
 	"os"
@@ -21,13 +22,26 @@ func textChunk(text string) io.Reader {
 	return &buffer
 }
 
-// func dumpChunk(chunk io.Reader) {
-// 	var length int32
-// 	binary.Read(chunk, binary.BigEndian, &length)
-// 	buffer := make([]byte, 4)
-// 	chunk.Read(buffer)
-// 	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
-// }
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+}
+
+func dumpChunk2(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+	if bytes.Equal(buffer, []byte("teXt")) {
+		rawText := make([]byte, length)
+		chunk.Read(rawText)
+		fmt.Println(string(rawText))
+	}
+}
 
 func readChunks(file *os.File) []io.Reader {
 	// チャンクを格納する配列
@@ -51,35 +65,35 @@ func readChunks(file *os.File) []io.Reader {
 }
 
 func main() {
+	file, err := os.Open("PNG_transparency_demonstration_secret.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	chunks := readChunks(file)
+	for _, chunk := range chunks {
+		dumpChunk2(chunk)
+	}
+
 	// file, err := os.Open("PNG_transparency_demonstration_1.png")
 	// if err != nil {
 	// 	panic(err)
 	// }
 	// defer file.Close()
-	// chunks := readChunks(file)
-	// for _, chunk := range chunks {
-	// 	dumpChunk(chunk)
+	// newFile, err := os.Create("PNG_transparency_demonstration_secret.png")
+	// if err != nil {
+	// 	panic(err)
 	// }
+	// defer newFile.Close()
+	// chunks := readChunks(file)
+	// // シグニチャ書き込み
+	// io.WriteString(newFile, "\x89PNG\r\n\x1a\n") // 先頭に必要なIHDRチャンクを書き込み
+	// io.Copy(newFile, chunks[0])
+	// // テキストチャンクを追加
+	// io.Copy(newFile, textChunk("Lambda Note++"))
 
-	file, err := os.Open("PNG_transparency_demonstration_1.png")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	newFile, err := os.Create("PNG_transparency_demonstration_secret.png")
-	if err != nil {
-		panic(err)
-	}
-	defer newFile.Close()
-	chunks := readChunks(file)
-	// シグニチャ書き込み
-	io.WriteString(newFile, "\x89PNG\r\n\x1a\n") // 先頭に必要なIHDRチャンクを書き込み
-	io.Copy(newFile, chunks[0])
-	// テキストチャンクを追加
-	io.Copy(newFile, textChunk("Lambda Note++"))
-
-	// 残りのチャンクを追加
-	for _, chunk := range chunks[1:] {
-		io.Copy(newFile, chunk)
-	}
+	// // 残りのチャンクを追加
+	// for _, chunk := range chunks[1:] {
+	// 	io.Copy(newFile, chunk)
+	// }
 }
